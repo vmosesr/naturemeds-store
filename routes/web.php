850e2c2934c6bfd3049279b3dbc30394c2ad0c96
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-03-28 00:43:12 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+use App\Http\Controllers\PagesController;
+
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
@@ -12,6 +14,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/products', [PagesController::class, 'index'])->name('products');
+Route::get('/clinics', [PagesController::class, 'index'])->name('clinics');
+Route::get('/checkouts', [PagesController::class, 'index'])->name('checkouts');
+Route::get('/articles', [PagesController::class, 'index'])->name('articles');
+Route::get('/support', [PagesController::class, 'index'])->name('support');
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
