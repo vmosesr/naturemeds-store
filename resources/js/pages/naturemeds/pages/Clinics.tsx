e43414860ad9828199365334c324e0a9f1befb53
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-03-28 00:42:14 +0300

    Create Clinics.tsx

---
+++ resources/js/pages/naturemeds/pages/Clinics.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Head } from "@inertiajs/react";
+
+export default function Products() {
+    return (
+        <div>
+            <Head title="Clinics" />
+            <h1 className="text-xl font-bold">Clinics Page</h1>
+            <p>Welcome to the Clinics page.</p>
+        </div>
+    );
+}
