5d954c3a9a572fba67ea94fc426577c2f84d5945
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-03-28 00:42:24 +0300

    Create Products.tsx

---
+++ resources/js/pages/naturemeds/pages/Products.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Head } from "@inertiajs/react";
+
+export default function Products() {
+    return (
+        <div>
+            <Head title="Products" />
+            <h1 className="text-xl font-bold">Products Page</h1>
+            <p>Welcome to the Products page.</p>
+        </div>
+    );
+}
