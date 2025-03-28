448d71252d1f5913f5fae77b1d2eac060134061d
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-03-28 14:52:16 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ClinicController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\SupportController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -15,11 +19,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-Route::get('/products', [PagesController::class, 'index'])->name('products');
-Route::get('/clinics', [PagesController::class, 'index'])->name('clinics');
-Route::get('/checkouts', [PagesController::class, 'index'])->name('checkouts');
-Route::get('/articles', [PagesController::class, 'index'])->name('articles');
-Route::get('/support', [PagesController::class, 'index'])->name('support');
+Route::get('/products', [ProductController::class, 'index'])->name('products');
+Route::get('/clinics', [ClinicController::class, 'index'])->name('clinics');
+Route::get('/checkouts', [CheckoutController::class, 'index'])->name('checkouts');
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles');
+Route::get('/support', [SupportController::class, 'index'])->name('support');
 
 
 require __DIR__.'/settings.php';
